d90dbbccf6c89976be0140aacb7c76425f224c6a
Joakim Algrøy <jal043@uib.no>
2018-04-21 22:34:32 +0200

    Moved draw methods from Slot to Disc where it belongs

--- src/inf101/v18/sem2/Disc.java
+++ src/inf101/v18/sem2/Disc.java
@@ -1,5 +1,56 @@
 package inf101.v18.sem2;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Disc {
-    HAL,GREEN,YELLOW,BLACK
+    HAL {
+        @Override
+        public void draw(GraphicsContext context, double x, double y, double width, double height){
+            drawImage(context, x, y, width, height, "HAL9000");
+        }
+    },GREEN {
+        @Override
+        public void draw(GraphicsContext context, double x, double y, double width, double height){
+            drawColor(context,x,y,width,height,Color.GREEN);
+        }
+    },YELLOW {
+        @Override
+        public void draw(GraphicsContext context, double x, double y, double width, double height){
+            drawColor(context,x,y,width,height,Color.YELLOW);
+        }
+    },BLACK {
+        @Override
+        public void draw(GraphicsContext context, double x, double y, double width, double height){
+            drawColor(context,x,y,width,height,Color.BLACK);
+        }
+    },WHITE {
+        @Override
+        public void draw(GraphicsContext context, double x, double y, double width, double height){
+            drawColor(context,x,y,width,height,Color.WHITE);
+        }
+    };
+
+    public abstract void draw(GraphicsContext context, double x, double y, double width, double height);
+
+    private static void drawColor(GraphicsContext context, double x, double y, double width, double height, Color color){
+        context.setFill(color);
+        context.fillOval(x, y, width, height);
+    }
+
+    private static void drawImage(GraphicsContext context, double x, double y, double width, double height, String filename){
+        context.drawImage(discImages.get(filename), x, y, width, height);
+    }
+
+    private static Map<String, Image> discImages = getDiscImages();
+
+    private static Map<String, Image> getDiscImages(){
+        Map<String, Image> discImages = new HashMap<>();
+        discImages.put("HAL9000", new Image("inf101/v18/sem2/images/HAL9000.png"));
+        return discImages;
+    }
 }
--- src/inf101/v18/sem2/datastructures/Slot.java
+++ src/inf101/v18/sem2/datastructures/Slot.java
@@ -2,14 +2,9 @@ package inf101.v18.sem2.datastructures;
 
 import inf101.v18.sem2.Disc;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Slot {
-    private static Map<String, Image> discImages = getDiscImages();
     private Disc disc;
     private int x;
     private int y;
@@ -41,37 +36,9 @@ public class Slot {
 
     public void draw(GraphicsContext context, double width, double height){
         if(disc == null){
-            drawColor(context,width,height,Color.WHITE);
+            Disc.WHITE.draw(context, width*(x+.1), height*(y+.1), .8*width, .8*height);
         } else {
-            switch (disc) {
-                case HAL:
-                    drawImage(context, width, height, "HAL9000");
-                    break;
-                case GREEN:
-                    drawColor(context, width, height, Color.GREEN);
-                    break;
-                case YELLOW:
-                    drawColor(context, width, height, Color.YELLOW);
-                    break;
-                case BLACK:
-                    drawColor(context, width, height, Color.BLACK);
-                    break;
-            }
+            disc.draw(context, width*(x+.1), height*(y+.1), .8*width, .8*height);
         }
-    }
-
-    private void drawColor(GraphicsContext context, double width, double height, Color color){
-        context.setFill(color);
-        context.fillOval(width*(x+.1), height*(y+.1), .8*width, .8*height);
-    }
-
-    private void drawImage(GraphicsContext context, double width, double height, String filename){
-        context.drawImage(discImages.get(filename), width*(x+.1),height*(y+.1),.8*width,.8*height);
-    }
-
-    private static Map<String, Image> getDiscImages(){
-        Map<String, Image> discImages = new HashMap<>();
-        discImages.put("HAL9000", new Image("inf101/v18/sem2/images/HAL9000.png"));
-        return discImages;
     }
 }

4c7528cb6deda8bcbf0c491072548b7a52f5b807
Joakim Algrøy <jal043@uib.no>
2018-04-27 19:52:31 +0200

    Cleaned up TitleScene

--- src/inf101/v18/sem2/gui/TitleScene.java
+++ src/inf101/v18/sem2/gui/TitleScene.java
@@ -12,41 +12,41 @@ import static inf101.v18.sem2.gui.GuiUtil.WIDTH;
 
 public class TitleScene extends Scene {
     private Stage stage;
-    private Group root;
     private Game game;
-
 
     public TitleScene(Stage stage, Group root, Game game){
         super(root, WIDTH, HEIGHT);
         this.stage = stage;
-        this.root = root;
         this.game = game;
-        initialize();
+
+        root.getChildren().addAll(GuiUtil.getBackgroundImage(), GuiUtil.getTitleImage(), btnMultiplayer(), btnAI());
     }
 
-    private void initialize(){
-        Button btnMultiplayer = new Button("Start 2-player game");
-        btnMultiplayer.setScaleX(1.5*SF);
-        btnMultiplayer.setScaleY(1.5*SF);
-        btnMultiplayer.setLayoutX(.4*WIDTH);
-        btnMultiplayer.setLayoutY(HEIGHT/2);
-        btnMultiplayer.setMinWidth(.2*WIDTH);
-        btnMultiplayer.setOnAction(actionEvent -> {
+    private Button btnMultiplayer(){
+        Button button = new Button("Start 2-player game");
+        button.setScaleX(1.5*SF);
+        button.setScaleY(1.5*SF);
+        button.setLayoutX(.4*WIDTH);
+        button.setLayoutY(HEIGHT/2);
+        button.setMinWidth(.2*WIDTH);
+        button.setOnAction(actionEvent -> {
             GetPlayerScene getPlayerScene = new GetPlayerScene(stage, new Group(), game, false);
             stage.setScene(getPlayerScene);
         });
+        return button;
+    }
 
-        Button btnAI = new Button("Start game vs HAL 9000");
-        btnAI.setScaleX(1.5*SF);
-        btnAI.setScaleY(1.5*SF);
-        btnAI.setLayoutX(.4*WIDTH);
-        btnAI.setLayoutY(3*HEIGHT/4);
-        btnAI.setMinWidth(.2*WIDTH);
-        btnAI.setOnAction(actionEvent -> {
+    private Button btnAI(){
+        Button button = new Button("Start game vs HAL 9000");
+        button.setScaleX(1.5*SF);
+        button.setScaleY(1.5*SF);
+        button.setLayoutX(.4*WIDTH);
+        button.setLayoutY(3*HEIGHT/4);
+        button.setMinWidth(.2*WIDTH);
+        button.setOnAction(actionEvent -> {
             GetPlayerScene getPlayerScene = new GetPlayerScene(stage, new Group(), game, true);
             stage.setScene(getPlayerScene);
         });
-
-        root.getChildren().addAll(GuiUtil.getBackgroundImage(), GuiUtil.getTitleImage(), btnMultiplayer, btnAI);
+        return button;
     }
 }
